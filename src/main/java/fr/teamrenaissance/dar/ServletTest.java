7e645ff23805d60ec68bac84f859acf769b03a53
jgamain <jeanne.gamain@gmail.com>
2017-10-04 23:30:50 +0200

    added maven project base structure with a test servlet

---
+++ src/main/java/fr/teamrenaissance/dar/ServletTest.java
@@ -0,0 +1,23 @@
+package fr.teamrenaissance.dar;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ServletTest")
+public class ServletTest extends HttpServlet {
+
+    //L'utilisateur envoie des données (via un formulaire...)
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    //L'utilisateur veut récupérer une page HTML
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.getServletContext().getRequestDispatcher("/WEB-INF/bonjour.jsp").forward(request, response);
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/bonjour.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Jeanne
+  Date: 04/10/2017
+  Time: 21:54
+  To change this template use File | Settings | File Templates.
+--%>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Test</title>
+</head>
+<body>
+<p>Bonjour à vous !</p>
+</body>
+</html>
