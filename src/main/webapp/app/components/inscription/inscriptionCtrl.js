2095679761177ce1bf6e5433011cac50793bb31f
jgamain <jeanne.gamain@gmail.com>
2017-11-06 17:18:58 +0100

    moved client side into webapp repo

---
+++ src/main/webapp/app/components/inscription/inscriptionCtrl.js
@@ -0,0 +1,3 @@
+app.controller('inscriptionCtrl', function($scope) {
+
+});
---
+++ src/main/webapp/app/shared/app.js
@@ -0,0 +1,32 @@
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function($routeProvider){
+	$routeProvider
+		.when("/accueil", {
+			templateUrl : "app/components/demandes/demandes.html",
+			controller : "demandesCtrl"
+		})
+		.when("/emprunter", {
+			templateUrl : "app/components/emprunter/emprunter.html",
+			controller : "emprunterCtrl"
+		})
+		.when("/mesprets", {
+			templateUrl : "app/components/mesPrets/mesPrets.html",
+			controller : "mesPretsController"
+		})
+		.when("/profil", {
+			templateUrl : "app/components/profil/profil.html",
+			controller : "profilCtrl"
+		})
+		.when("/connexion", {
+			templateUrl : "app/components/connexion/connexion.html",
+			controller : "connexionCtrl"
+		})
+		.when("/inscription", {
+			templateUrl : "app/components/inscription/inscription.html",
+			controller : "inscriptionCtrl"
+		})
+		.otherwise({
+			templateUrl : "app/components/demandes/demandes.html",
+			controller : "demandesCtrl"
+		});
+});
