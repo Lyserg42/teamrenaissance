e94628ef854d11fb9b95099059a4eefc2bcadd84
Unknown <julien.henry@gmx.com>
2017-11-07 00:01:03 +0100

    Ajout Profil basique. Correction de la structure de profil.json

--- src/main/webapp/app/components/profil/profilCtrl.js
+++ src/main/webapp/app/components/profil/profilCtrl.js
@@ -1,3 +1,11 @@
-app.controller('profilCtrl', function($scope) {
+app.controller('profilCtrl', function($scope, $http) {
+
+	$http.get("app/components/profil/profil.json").then(function(response){
+
+                /* On stocke les données récupérées*/
+            	$scope.profil = response.data;
+
+
+	});
 
 });
