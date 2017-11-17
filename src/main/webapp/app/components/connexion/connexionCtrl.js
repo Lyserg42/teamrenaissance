6d7dc992b0f5a013d84994e6ce16471dd70587eb
Unknown <julien.henry@gmx.com>
2017-11-17 17:01:57 +0100

    Gestion des erreurs serveurs dans connexion

--- src/main/webapp/app/components/connexion/connexionCtrl.js
+++ src/main/webapp/app/components/connexion/connexionCtrl.js
@@ -1,9 +1,11 @@
 app.controller('connexionCtrl', function($scope, $http, $route) {
 	 
 	$scope.erreurConnexion = false;
+    $scope.succesConnexion = false;
 
 	 $scope.connexion = function () {
 	 	$scope.erreurConnexion = false;
+        $scope.succesConnexion = false;
 	 	
         var data = {typeRequest:"connexion", login: $scope.login, password: $scope.password};
         var dataJSON = JSON.stringify(data);
@@ -12,11 +14,19 @@ app.controller('connexionCtrl', function($scope, $http, $route) {
 
       	$http.post("/user", dataJSON).then(
             function success(response){
+                $scope.succesConnexion = true;
                 console.log("succes");
                 console.log(response.status);
           		console.log(response.data);
             },
             function error(response){
+                $scope.erreurConnexion = true;
+                if(response.status != -1){
+                    $scope.raisonEchecConnexion = "Combinaison Login / Mot de passe incorrecte.";
+                }
+                else{
+                    $scope.raisonEchecConnexion = "Impossible de contacter le serveur.";
+                }
                 console.log("erreur");
                 console.log(response.status);
                 console.log(response.data);
